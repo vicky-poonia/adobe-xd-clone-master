6b456a50bcb54a24af5bdfd19b6c61986804371f
Vicky Poonia <114798756+vicky-poonia@users.noreply.github.com>
2023-09-10 02:04:45 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+ import Form from './Compoants/Form/Form';
+// import Test from './Compoants/test/test';
+
+function App() {
+  return (
+    <div className="App">
+      <Form/>
+      {/* <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit..
+        nulla ut totam quidem, aspernatur et similique asperiores, culpa reprehenderit. Reiciendis?
+      </p> */}
+      {/* <Test/> */}
+
+    </div>
+  );
+}
+
+export default App;
